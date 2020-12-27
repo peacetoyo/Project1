c9a7c6cb3dcd31648d2bcb38cd6a8488a0de43ab
Gabriel Toyo <gabriel.toyo@outlook.com>
2020-12-27 10:59:14 -0600

    Update Message

--- src/Salesforce.java
+++ src/Salesforce.java
@@ -8,9 +8,9 @@ public class Salesforce {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver.exe" );
          WebDriver driver=new ChromeDriver();
-         driver.get("https://login.salesforce.com/");
+         driver.get("https://login.Facebook.com/");
          System.out.println(driver.getTitle());
-         driver.findElement(By.id("username")).sendKeys("gabrieltoyo@outlook.com");
+         driver.findElement(By.id("username")).sendKeys("gabriel.toyo@outlook.com");
          driver.findElement(By.id("password")).sendKeys("Il0vemylife45");
          driver.findElement(By.id("Login")).click();
          driver.findElement(By.linkText("Home Tab")).click();
